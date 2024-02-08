86a355925d998365342891eae6cb366231b836c9
bmxcmx <cadenwhitfield86@gmail.com>
2024-02-07 18:47:49 -0600

    Initial commit

---
+++ src/main/java/frc/robot/Commands/Auto/TestAuto.java
@@ -0,0 +1,13 @@
+package frc.robot.Commands.Auto;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+
+public class TestAuto extends SequentialCommandGroup{
+    
+}
